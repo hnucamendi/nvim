ecb5f4651de5581de1f16e935e2d6b27aaac0687
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-01-18 23:27:20 -0500

    still trying to add treesitter

--- lua/hnucamendi/plugins/treesitter.lua
+++ lua/hnucamendi/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-vim.defer_fn(function()
+return vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'lua', 'go', 'c', 'cpp', 'python', 'tsx', 'javascript', 'jsx', 'typescript', 'vimdoc', 'vim', 'bash', 'terraform', 'hcl', 'json', 'markdown', 'sql' },
