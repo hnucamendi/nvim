0ecd5fc1af38cbf64a8c5f1fca3793a69e909323
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-01-20 11:39:45 -0500

    test

--- lua/hnucamendi/lazy.lua
+++ lua/hnucamendi/lazy.lua
@@ -13,6 +13,23 @@ vim.opt.rtp:prepend(lazypath)
 
 return require("lazy").setup({
 
+      {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
