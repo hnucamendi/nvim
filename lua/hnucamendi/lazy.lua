951b185fa236dc83fd7d4135c6923a7b120a2838
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-01-20 13:04:45 -0500

    adding lsp

--- lua/hnucamendi/lazy.lua
+++ lua/hnucamendi/lazy.lua
@@ -12,6 +12,33 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 return require("lazy").setup({
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+        -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
 
       {
     -- Autocompletion
