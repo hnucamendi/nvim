1d7db4a8e39c72b7866163f8973cd9a60281cd37
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-01-21 20:37:53 -0500

    gopls

--- lua/hnucamendi/plugins/lsp.lua
+++ lua/hnucamendi/plugins/lsp.lua
@@ -2,8 +2,7 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -19,12 +18,27 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'go'},
+  ensure_installed = {'tsserver', 'gopls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+
+    gopls = function()
+      require('lspconfig').gopls.setup({
+        settings = {
+          gopls = {
+            analyses = { unusedparams = true, unreachable = true },
+            completeUnimported = true,
+            usePlaceholders = true,
+            staticcheck = true,
+            gofumpt = true,
+            completeFunctionCalls = true,
+          },
+        },
+      })
     end,
   }
 })
