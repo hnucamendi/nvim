7e7cfba595b3b064c394d14fcd3eea68f04073ab
hnucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-04-19 18:26:20 -0400

    adding formatter for react / jsx files

--- init.lua
+++ init.lua
@@ -590,7 +590,7 @@ require('lazy').setup({
   { -- Autoformat
     'stevearc/conform.nvim',
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
@@ -608,8 +608,10 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { { 'prettier', 'prettierd' } },
+        javascriptreact = { { 'prettier', 'prettierd' } },
         go = { { 'goimports', 'gofmt' } },
+        json = { { 'prettierd', 'prettier' } },
       },
     },
   },
