1418e08de3edfda74b461fe761d56474b45aaa7f
hnucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-04-23 06:47:49 -0400

    adding linter and formatters --no-verify

--- init.lua
+++ init.lua
@@ -592,7 +592,9 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettier', 'prettierd' } },
+        typescript = { { 'prettier', 'prettierd' } },
         javascriptreact = { { 'prettier', 'prettierd' } },
+        typescriptreact = { { 'prettier', 'prettierd' } },
         go = { { 'goimports', 'gofmt' } },
         json = { { 'prettierd', 'prettier' } },
       },
