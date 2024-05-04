a64181e13ea13afc732732f54c7c60637615faf4
hnucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-05-04 12:28:47 -0400

    remove auto linting func

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -51,13 +51,13 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          require('lint').try_lint()
-        end,
-      })
+      -- local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      --   group = lint_augroup,
+      --   callback = function()
+      --     require('lint').try_lint()
+      --   end,
+      -- })
 
       vim.keymap.set('n', '<leader>ll', function()
         lint.try_lint()
