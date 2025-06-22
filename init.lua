167956e1423a906fdc9c6244acbb9716b4a5d9e5
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-06-21 23:13:18 -0400

    removing telescope

--- init.lua
+++ init.lua
@@ -73,10 +73,6 @@ require("lazy").setup({
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
-	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-	-- Or use telescope!
-	-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-	-- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
