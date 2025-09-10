dc57fbd1192839b20d0721c8d2ef8b9f32fe3a4d
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-09-10 06:44:29 -0400

    adding gofumpt

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -210,13 +210,14 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
-      "prettier",
-      "prettierd",
-      "markdownlint",   -- (markdownlint-cli)
-      "clang-format",
-      "goimports",
-      "black",
-      "biome",
+			"prettier",
+			"prettierd",
+			"markdownlint", -- (markdownlint-cli)
+			"clang-format",
+			"goimports",
+			"black",
+			"biome",
+			"gofumpt",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
