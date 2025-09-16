26a7fa3aec4b3cf2ce926d66b8cf3cc644c6c836
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-09-16 19:59:51 -0400

    saving

--- lua/plugins/extend-catppuccin.lua
+++ lua/plugins/extend-catppuccin.lua
@@ -5,19 +5,11 @@ return {
     priority = 1000,
     opts = {
       flavour = "auto",
-      background = { light = "latte", dark = "frappe" },
-      transparent_background = false,
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
-          end
-        end,
+      background = {
+        light = "latte",
+        dark = "frappe",
       },
+      transparent_background = false,
     },
   },
   {
@@ -25,4 +17,3 @@ return {
     opts = { colorscheme = "catppuccin" },
   },
 }
-
