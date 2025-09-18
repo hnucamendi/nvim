37fa4df4f14d3d033fc512bf113dd8de13f65c8f
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-09-18 08:35:51 -0400

    saving

--- lua/plugins/extend-catppuccin.lua
+++ lua/plugins/extend-catppuccin.lua
@@ -10,10 +10,6 @@ return {
         dark = "frappe",
       },
     },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
-    end,
   },
   {
     { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
